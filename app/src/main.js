fc2e0be7f70fd8713f34a54a68e903fe53af21bb
Marina <marinasgv@hotmail.com>
2022-08-22 19:28:10 -0300

    atualização

--- app/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '@/assets/css/style.css'
-import router from './router'
-import store from './store'
-
-window.app = createApp(App).use(store).use(router).mount( '#app')
