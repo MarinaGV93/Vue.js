b090fa578842546520ac9ab0e10b405c06334f44
Audiomind-official <marinasgv@hotmail.com>
2022-11-21 16:21:15 -0300

    Composition API -Setup

---
+++ Composition API/app/src/components/Products/AppProduct.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <AppProduct/>
+    {{name}}
+</template>
+
+<script>
+import AppProduct from '@/components/Products/AppProduct.vue';
+
+
+    export default{
+        name: 'App',
+        components:{
+            AppProduct,
+        },
+        setup(){
+            // Nao é reativo
+            const name = "Marina"
+
+            return{
+                name
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
---
+++ Composition API/app/src/components/Products/CalculateDiscountMixin.vue
@@ -0,0 +1,9 @@
+<script>
+    export default{
+        methods: {
+            calculateDiscount(){
+                console.log('calculateDiscount')
+            }
+        }
+    }
+</script>
